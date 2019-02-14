0d5e89a820d805af69835d3269d3ec45ad02716e
Kerem Güneş <k-gun@mail.com>
2019-02-14 23:12:49 +0300

    Bump froq\exception.

--- src/CollectionException.php
+++ src/CollectionException.php
@@ -26,8 +26,6 @@ declare(strict_types=1);
 
 namespace froq\collection;
 
-use froq\exception\Exception;
-
 /**
  * Collection exception.
  * @package froq\collection
@@ -35,5 +33,5 @@ use froq\exception\Exception;
  * @author  Kerem Güneş <k-gun@mail.com>
  * @since   1.0
  */
-final class CollectionException extends Exception
+final class CollectionException extends \Exception
 {}
