3cb63d607881871d49e4d0282fe1833c056becda
Kerem Güneş <qeremy@gmail.com>
2022-02-04 07:30:26 +0300

    * AbstractCollection: use ArrayTrait [dropping XArray], optimize.

--- src/AbstractCollection.php
+++ src/AbstractCollection.php
@@ -7,7 +7,10 @@ declare(strict_types=1);
 
 namespace froq\collection;
 
-use froq\common\object\XArray;
+use froq\common\interface\{Listable, Arrayable, Objectable, Jsonable, Yieldable,
+    Iteratable, IteratableReverse};
+use froq\common\trait\{ArrayTrait, ReadOnlyTrait};
+use froq\util\Util;
 
 /**
  * Abstract Collection.
@@ -17,5 +20,31 @@ use froq\common\object\XArray;
  * @author  Kerem Güneş
  * @since   4.0
  */
-abstract class AbstractCollection extends XArray
-{}
+abstract class AbstractCollection implements Listable, Arrayable, Objectable, Jsonable, Yieldable,
+    Iteratable, IteratableReverse, \Iterator, \Countable, \JsonSerializable
+{
+    /** @see froq\common\trait\ArrayTrait */
+    use ArrayTrait;
+
+    /** @see froq\common\trait\ReadOnlyTrait */
+    use ReadOnlyTrait;
+
+    /** @var array */
+    protected array $data = [];
+
+    /**
+     * Constructor.
+     *
+     * @param iterable|null $data
+     * @param bool|null     $readOnly
+     */
+    public function __construct(iterable $data = null, bool $readOnly = null)
+    {
+        if ($data) {
+            $data = Util::makeArray($data, deep: false);
+            $this->setData($data);
+        }
+
+        $this->readOnly($readOnly);
+    }
+}
