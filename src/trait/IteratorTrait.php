8ce8d423ef347448e33f042feb22afa75af28364
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-11-07 22:12:59 +0300

    - trait.IteratorTrait: fix valid() [live-lock recursion issue].

--- src/trait/IteratorTrait.php
+++ src/trait/IteratorTrait.php
@@ -10,6 +10,23 @@ namespace froq\collection\trait;
 /**
  * A trait, provides related methods for the classes implementing `Iterator` interface.
  *
+ * Note: While iterating a user object of this trait and key / index access needed in a
+ * loop (eg: to get "next" or "prev" item), a copy of this object (clone) should be used,
+ * instead of making look ups like:
+ *
+ * Bad usage:
+ * ```
+ * foreach ($iter as $i => $curr)
+ *   $next = $iter[$i+1];
+ * ```
+ *
+ * Good usage:
+ *```
+ * $copy = clone $iter;
+ * foreach ($iter as $i => $curr)
+ *   $next = $copy[$i+1];
+ *  ```
+ *
  * @package froq\collection\trait
  * @object  froq\collection\trait\IteratorTrait
  * @author  Kerem Güneş
@@ -17,21 +34,34 @@ namespace froq\collection\trait;
  */
 trait IteratorTrait
 {
+    /** @internal */
+    private int $__pointer = 0;
+
     /** @inheritDoc Iterator */
     public function rewind(): void
     {
+        $this->__pointer = count($this->data);
+
         reset($this->data);
     }
 
     /** @inheritDoc Iterator */
     public function valid(): bool
     {
-        return key($this->data) !== null;
+        return $this->__pointer > 0;
+
+        // @cancel: Since key() does NOT move the pointer,
+        // relying on this causes live-lock (recursions) in
+        // foreach() blocks, when this kind of usage present:
+        // foreach ($iter as $i => $value) { $next = $iter[$i+1]; }
+        // return key($this->data) !== null;
     }
 
     /** @inheritDoc Iterator */
     public function next(): void
     {
+        $this->__pointer--;
+
         next($this->data);
     }
 
