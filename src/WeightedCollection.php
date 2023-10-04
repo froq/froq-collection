6a154c3f812d0f7daf2cb73afd1c4b963129a268
Kerem Güneş <1508306+okerem@users.noreply.github.com>
2023-10-05 00:28:11 +0300

    WeightedCollection: optimize.

--- src/WeightedCollection.php
+++ src/WeightedCollection.php
@@ -26,16 +26,16 @@ class WeightedCollection extends AbstractCollection
     }
 
     /**
-     * Select items (by optionally given min-weight / max-weight).
+     * Select items (by optionally given min/max weight).
      *
-     * @param  float|null $minWeight
-     * @param  float|null $maxWeight
+     * @param  int|float|null $minWeight
+     * @param  int|float|null $maxWeight
      * @return array|null
      */
-    public function selectBy(float $minWeight = null, float $maxWeight = null): array|null
+    public function selectBy(int|float $minWeight = null, int|float $maxWeight = null): array|null
     {
         return $this->filterize(function ($item) use ($minWeight, $maxWeight) {
-            // There is nothing to do without weight.
+            // There is nothing to do without a "weight" field.
             if (!is_array($item) || !isset($item['weight'])) {
                 return false;
             }
@@ -54,9 +54,6 @@ class WeightedCollection extends AbstractCollection
 
     /**
      * Filter self data items by given callback.
-     *
-     * @param  callable $calback
-     * @return array|null
      */
     private function filterize(callable $calback): array|null
     {
@@ -91,10 +88,6 @@ class WeightedCollection extends AbstractCollection
 
     /**
      * Get a random number by given min/max directives.
-     *
-     * @param  float $min
-     * @param  float $max
-     * @return float
      */
     private function randomize(float $min, float $max): float
     {
